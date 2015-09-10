e7475127acfba95422902601a8a1ed87d74c438b
Steven Gosseling <stevengosseling@hotmail.com>
2015-09-10 11:03:28 -0700

    added some code to send the actual command

--- Code/CommunicationProtocol/testSteven/send_command.c
+++ Code/CommunicationProtocol/testSteven/send_command.c
@@ -1,5 +1,17 @@
 #include "static.h"
 
 int send_command(int board,char label, char command){
-	return 0;
+	int done = 0;
+
+	packet p;
+	p.label = label;
+	p.command = command;
+
+	do
+	{
+	done = write(board, &p, sizeof(p));
+	}
+	while(!done);
+
+	return done;
 }
