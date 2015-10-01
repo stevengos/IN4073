108bde067d90236c60ff6097aeb42543a921d38b
Sinai <sinai4l@gmail.com>
2015-10-01 23:10:14 +0200

    UpdateOne after compiling

--- Code/MAIN/BOARD/butterworth.c
+++ Code/MAIN/BOARD/butterworth.c
@@ -5,20 +5,25 @@
 
 #include "butterworth.h"
 
+int i, j;
+int x0_1, x0_2, x0_3;
+int x1_1, x1_2, x1_3;
+int x2_1, x2_2, x2_3; 
+int y1_1, y1_2, y1_3; 
+int y2_1, y2_2, y2_3;
+ 
+
+
 void butter_second()
 {
+  
+  struct butter_s new_data;
+  
   if (Sample ==99)
   {
     reset_sample();
   }
-  int i, j;
-  int x0_1, x0_2, x0_3;
-  int x1_1, x1_2, x1_3;
-  int x2_1, x2_2, x2_3; 
-  int y1_1, y1_2, y1_3; 
-  int y2_1, y2_2, y2_3;
- 
-  struct butter_s new_data;
+
 
  
 
