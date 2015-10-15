fbbe23d1eca9a343bce2979089c9f8f35c3262d0
Sinai <sinai4l@gmail.com>
2015-10-15 18:49:34 +0200

    latest update Tuesday-13-10-2015

--- Code/MAIN/BOARD/butterworth.h
+++ Code/MAIN/BOARD/butterworth.h
@@ -18,6 +18,8 @@
 #define b2 3208
 
 
+extern struct drone qr;
+
 int Sample=0;
 
 struct butter_s  //Structure that will contain the buffered data
@@ -28,8 +30,8 @@ struct butter_s  //Structure that will contain the buffered data
    short int sr;
 };
 
-short x[3][3];
-short y[3][3];
+short int x[3][3];
+short int y[3][3];
 
 void butter_second(void);
 #endif//BUTTERWORTH_H
